dd5070dc8c964c2ee7304812f6a2b47b789d4b04
nazrul-avash <nazrul.avash@gmail.com>
2026-02-22 00:04:53 +0600

    button action partially done

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let allCardSection = document.getElementById("all-card");
 const allFilterBtn = document.getElementById("all-filter-btn");
 const interviewFilterBtn = document.getElementById("interview-filter-btn");
 const rejectedFilterBtn = document.getElementById("rejected-filter-btn");
+const mainContainer = document.querySelector("main");
 
 function  calculateCount() {
     total.innerText = allCardSection.children.length;
@@ -29,3 +30,35 @@ function toggleStyle(id){
 
 }
 calculateCount()
+
+mainContainer.addEventListener("click",function(event){
+    if(event.target.classList.contains('btn-interview')){
+        const parentNode = event.target.parentNode;
+        const companyName = parentNode.querySelector(".company-name").innerText;
+        const designation = parentNode.querySelector(".designation").innerText;
+        const nature = parentNode.querySelector(".nature").innerText;
+        
+        const desc = parentNode.querySelector(".desc").innerText;
+        if (!parentNode.querySelector(".status")){
+            const statusBtn = document.createElement("button");
+            statusBtn.classList.add("status", "btn", "btn-outline", "bg-gray-200", "mb-2", "w-fit");
+            statusBtn.innerText=event.target.innerText;
+            const afterChild = parentNode.children[2];
+            afterChild.after(statusBtn);
+        }
+        const status = parentNode.querySelector(".status").innerText;
+        const jobInfo ={
+            companyName,
+            designation,
+            nature,
+            status,
+
+        };
+        const jobExist = interviewList.find(item =>item.companyName === jobInfo.companyName);
+        if (!jobExist){
+            interviewList.push(jobInfo);
+        }
+        rejectedList = rejectedList.filter(item =>item.companyName != jobInfo.companyName);
+   
+    }
+});
